193d41b46b7049f12b911de5672b51fe9ffedc53
Liping Mechling <65612959+Lime5005@users.noreply.github.com>
2022-03-28 17:03:48 +0200

    Service Injection

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
+import { LoggingService } from '../logging.service'; //1
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]  //2
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private loggingService: LoggingService) {} //3
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    //console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status); //4
   }
 }
